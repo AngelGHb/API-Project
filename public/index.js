dc9c2d9140bfe7e9cee5fca192cdc6873327e2ca
Angel Solano <angels31927@gmail.com>
2025-07-24 14:06:38 -0400

    Adding css, base html, and base js functionality for button

---
+++ public/index.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const playButton = document.querySelector('.play-button');
+  playButton.addEventListener('click', () => {
+    console.log('play pressed')
+  })
+});
