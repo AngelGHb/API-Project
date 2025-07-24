89ef1b0c61d446a8415098b820ef288db935ded4
Zhihua Li <e925874478@gmail.com>
2025-07-24 15:45:21 -0400

    connecting to api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,28 +10,28 @@ const app = express();
 const port = 3000;
 const API_URL = "https://deckofcardsapi.com/api/deck/";
 
-app.set("views engine", "views");
-app.set(
-  "views",
-  "/Users/ttp/Documents/GitHub/A Simple Card Game/Untitled/API-Project/views"
-);
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs");
+  res.render("index.ejs", { content: [], error: null });
 });
 
 app.post("/play", async (req, res) => {
-  try {}
+  try {
     const result = await axios.get(API_URL + "new/");
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-    console.log(result.data);
+    const cards = await axios.get(
+      API_URL + result.data.deck_id + "/draw/?count=2"
+    );
+
+    res.render("index.ejs", { content: cards.data.cards });
+    console.log(cards.data.cards);
   } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    res.render("index.ejs", { content: error.response.data });
   }
 });
-
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
