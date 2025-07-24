be74e687f064de73c3d89ea9f512051be1a43e1d
Zhihua Li <e925874478@gmail.com>
2025-07-24 14:52:34 -0400

    api attemp

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -7,12 +8,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+const API_URL = "https://deckofcardsapi.com/api/deck/";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {});
+
+app.post("/play", async (req, res) => {
+  const searchId = req.body.id;
+  try {
+    const result = await axios.get(API_URL + "new/");
+    // res.render("index.ejs", { content: JSON.stringify(result.data) });
+    console.log(result.data);
+  } catch (error) {
+    // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.listen(port, () => {
