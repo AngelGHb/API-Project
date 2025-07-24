351711f4ab93180a303e384748497c01ea924ed5
Angel Solano <angels31927@gmail.com>
2025-07-24 14:29:34 -0400

    base logic for game buttons

--- public/index.js
+++ public/index.js
@@ -1,11 +1,30 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const playButton = document.querySelector('.play-button');
+  let hitButton = null
+  let standButton = null
+//   game start/ play pressed
   playButton.addEventListener('click', () => {
-    const hitButton =  document.createElement('button')
+
+    // show player hand unhide game buttons 
+    hitButton =  document.createElement('button')
     hitButton.textContent = 'Hit'
     hitButton.className = 'hit-button'
-    hitButton.addEventListener("click")
+
+    standButton = document.createElement('button')
+    standButton.textContent ='Stand'
+    standButton.classname = 'stand-button'
   })
+
+//   logic to check if the buttons exist/ can be clicked yet
+  if (standButton != null) {
+    standButton.addEventListener('click', () => {
+
+    })
+  }
+
+  if (hitButton != null) {
+    standButton.addEventListener('click', () => {
+
+    })
+  }
 });
